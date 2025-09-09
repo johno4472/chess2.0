caeb371ac8b33c813aaf0b74bafb090ad502a909
John <johnnephiolson@gmail.com>
2025-09-09 12:53:14 -0600

    Added Hash and Equals overrides and passed final ChessGame tests (excluding extra credit)

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -3,6 +3,7 @@ package chess;
 import java.io.Console;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -231,5 +232,19 @@ public class ChessGame {
     public ChessBoard getBoard() {
 
         return chessBoard;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return teamTurn == chessGame.teamTurn && Objects.equals(chessBoard, chessGame.chessBoard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamTurn, chessBoard);
     }
 }
